fe707193d2f2a16f16796434031759a05bf5ae9e
abhinay-brtneura <abhinay.bs@brtneura.com>
2025-09-10 11:59:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from google.auth import default
+from google.cloud import firestore
+
+app = Flask(__name__)
+
+@app.route("/check-auth")
+def check_auth():
+    try:
+        # Check what credentials Cloud Run is using
+        creds, project = default()
+        client = firestore.Client()
+        docs = client.collection("test").limit(1).get()
+
+        return jsonify({
+            "auth_type": str(type(creds)),
+            "project_id": project,
+            "firestore_access": "success",
+            "sample_doc": [doc.to_dict() for doc in docs]
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
